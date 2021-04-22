8bcbde4f90883c623a1ec122fde7339277d514eb
danielle <danielle.teves@gsfe.tupcavite.edu.ph>
2021-04-22 23:21:10 +0800

    first test to pass

--- CharityApp/tests.py
+++ CharityApp/tests.py
@@ -1,3 +1,11 @@
+from django.urls import resolve
 from django.test import TestCase
+from CharityApp.views import DonationForm
+
+class DonationFormTest(TestCase):
+
+	def test_root_url_resolves_to_donationfrom_views(self):
+		found = resolve ('/')
+		self.assertEqual(found.func, DonationForm)
 
 # Create your tests here.
--- ProjectFinal/urls.py
+++ ProjectFinal/urls.py
@@ -1,3 +1,11 @@
+from django.conf.urls import url 
+from CharityApp import views
+
+urlpatterns = [
+    url(r'^$', views.DonationForm, name='donationform'),
+]
+
+
 """ProjectFinal URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,9 +21,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+# from django.contrib import admin
+# from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
