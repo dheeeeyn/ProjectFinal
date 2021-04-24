68c5725d4f86069ee99828e7fd12b8ce48e8a5c2
danielle <danielle.teves@gsfe.tupcavite.edu.ph>
2021-04-24 10:57:03 +0800

    unit test is passing

--- CharityApp/views.py
+++ CharityApp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+
+def DonationForm(request):
+	return render(request,'donationform.html')
 
 # Create your views here.
 # DonationForm = None
-def DonationForm(request):
-	# pass
-	return HttpResponse('<html><head><title>Donation Form</title><!-- padding-left: 25rem; --></head><body style="text-align:center; margin-top: 150px; margin-bottom: 300px; background-image: linear-gradient(to top, #9890e3 0%, #b1f4cf 100%);"><div class="contact" style="display: flex; align-items: center; justify-content: center;" ><div class="contact-box" style=" width:50%; border:black; border-width:3px; border-style:solid;"><h1 style="font-size: 3rem;"> Make Donations Now!!! </h1><hr style="width:30%;"><br><form><h3>Amount</h3><input type="radio" id="100" name="amount"  value="100" required><label for="100"> &#8369; 100</label><input type="radio" id=3300" name="amount" value="300" ><label for="100"> &#8369; 300</label><input type="submit"  id="btnSend" value="Donate"></form></div></div></body></html>')                  
+# def DonationForm(request):
+# 	# pass
+# 	return HttpResponse('<html><head><title>Donation Form</title><!-- padding-left: 25rem; --></head><body style="text-align:center; margin-top: 150px; margin-bottom: 300px; background-image: linear-gradient(to top, #9890e3 0%, #b1f4cf 100%);"><div class="contact" style="display: flex; align-items: center; justify-content: center;" ><div class="contact-box" style=" width:50%; border:black; border-width:3px; border-style:solid;"><h1 style="font-size: 3rem;"> Make Donations Now!!! </h1><hr style="width:30%;"><br><form><h3>Amount</h3><label style="font-weight: 900;" for="name"> Name:</label><br><br><input type="name" id="name" name="name" placeholder="Name" style="width:20rem;"required><br><br><label style="font-weight: 900;" for="email"> Email:</label><br><br><input type="email" id="email" name="email" placeholder="input valid email.." style="width:20rem;"required><br><br><label style="font-weight: 900;" for="message">Message:</label><br><textarea id="message" name="message" placeholder="Write something... (optional)" rows=15 style="height:8rem; width:20rem;"></textarea><br><br><input type="submit"  id="btnSend" value="Donate"></form></div></div></body></html>')                  
--- FunctionalTest.py
+++ FunctionalTest.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 import unittest
 
+from selenium.webdriver.common.keys import Keys
+import time
+
 class DonationForm(unittest.TestCase):
 
 	def setUp(self):
@@ -9,10 +12,49 @@ class DonationForm(unittest.TestCase):
 	# def tearDown(self):
 	# 	self.browser.quit()
 
-	def test_browser_title(self):
+	# def test_browser_title(self):
+	# 	self.browser.get('http://localhost:8000')
+	# 	self.assertIn('Donation Form', self.browser.title)
+	# 	#self.fail('Finish the test noww')
+
+	def test_start_list_and_retrive_it(self):
 		self.browser.get('http://localhost:8000')
 		self.assertIn('Donation Form', self.browser.title)
-		#self.fail('Finish the test noww')
+		headerText = self.browser.find_element_by_tag_name('h1').text
+		headerText2 = self.browser.find_element_by_tag_name('h3').text
+		paragraph = self.browser.find_element_by_id("p1").text
+		paragraph2 = self.browser.find_element_by_id("p2").text
+		headerText3 = self.browser.find_element_by_tag_name('h2').text
+		headerText4 = self.browser.find_element_by_id("hA").text
+		self.assertIn('Project Malasakit', headerText)
+		self.assertIn('Make Donations Now', headerText2) 
+		self.assertIn('Raised', paragraph)
+		self.assertIn('Goal', paragraph2) 
+		self.assertIn('Donation Form', headerText3)
+		self.assertIn('Amount', headerText4)
+		#Amount
+		btnAmount = self.browser.find_element_by_id('1000')
+		#Contact
+		inputName = self.browser.find_element_by_id('name')
+		inputEmail = self.browser.find_element_by_id('email')
+		inputMessage = self.browser.find_element_by_id('message')
+		btnSubmit = self.browser.find_element_by_id('btnDonate')
+		self.assertEqual(inputName.get_attribute('placeholder'),'Your name..')
+		self.assertEqual(inputEmail.get_attribute('placeholder'),'input valid email..')
+		self.assertEqual(inputMessage.get_attribute('placeholder'),'Write something... (optional)')
+		btnSubmit.click() # to check if the required attribute in html is working
+		self.assertEqual(btnAmount.get_attribute('value'),'1000')
+		time.sleep(3)
+		btnAmount.click()
+		time.sleep(3)
+		inputName.send_keys('danene')
+		time.sleep(2)
+		inputEmail.send_keys('danene@gmail.com')
+		time.sleep(2)
+		inputMessage.send_keys('PAPASA AKO SA WEBDEV2')
+		time.sleep(2)
+		btnSubmit.click()
+		time.sleep(2)
 
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
